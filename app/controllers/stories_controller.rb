1b3deaa87eb80b5d0d5131dec5e627a97dbc1618
ryoken nakayama <ryoken7090@gmail.com>
2020-06-09 17:58:27 +0900

    詳細画面作成

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,16 +1,24 @@
 class StoriesController < ApplicationController
   def index
   end
+
   def new
     @story = Story.new
   end
+
   def create
     Story.create(story_params)
     redirect_to stories_path
   end
+
   def index
     @stories = Story.all
   end
+
+  def show
+    @story = Story.find(params[:id])
+  end
+
   private
   def story_params
     params.require(:story).permit(:content, :image)
