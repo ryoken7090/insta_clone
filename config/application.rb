32316227ecfe8ecfac2b30950057b78f57acc879
ryoken nakayama <ryoken7090@gmail.com>
2020-06-09 16:19:41 +0900

    intsta_cloneデータの作り直し、日本語化の解除

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,9 @@ Bundler.require(*Rails.groups)
 module InstaClone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.time_zone = 'Tokyo'
-        config.active_record.default_timezone = :local
-        config.i18n.default_locale = :ja
+
+    # config.time_zone = 'Tokyo'
+    #     config.active_record.default_timezone = :local
 
     config.load_defaults 5.2
 
