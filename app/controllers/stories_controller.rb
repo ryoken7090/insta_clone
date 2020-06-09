4a5ec7b78e0491b8abff624fa734c78bec6419f5
ryoken nakayama <ryoken7090@gmail.com>
2020-06-09 18:35:58 +0900

    編集機能と共通画面formの作成・共通処理set_storyの設定

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,7 @@
 class StoriesController < ApplicationController
+  before_action :set_story, only: [:show, :edit, :update]
   def index
+    @stories = Story.all
   end
 
   def new
@@ -7,7 +9,6 @@ class StoriesController < ApplicationController
   end
 
   def create
-    # Story.create(story_params)
     @story = Story.new(story_params)
     if @story.save
       redirect_to stories_path
@@ -16,16 +17,26 @@ class StoriesController < ApplicationController
     end
   end
 
-  def index
-    @stories = Story.all
+  def show
   end
 
-  def show
-    @story = Story.find(params[:id])
+  def edit
+  end
+
+  def update
+    if @story.update(story_params)
+      redirect_to stories_path
+    else
+      render :edit
+    end
   end
 
   private
   def story_params
     params.require(:story).permit(:content, :image)
+  end
+
+  def set_story
+    @story = Story.find(params[:id])
   end
 end
