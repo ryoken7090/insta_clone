02014fe45741ca3affb314959364c1acde7be002
ryoken nakayama <ryoken7090@gmail.com>
2020-06-10 11:58:24 +0900

    userコントローラ、モデル、テーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'
   resources :stories do
     collection do
       post :confirm
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_050145) do
+ActiveRecord::Schema.define(version: 2020_06_10_025335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2020_06_09_050145) do
   create_table "stories", force: :cascade do |t|
     t.text "image"
     t.text "content"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
