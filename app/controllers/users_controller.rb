02ed003f9188f0f76aaef59d31c9d5f4195048c6
ryoken nakayama <ryoken7090@gmail.com>
2020-06-10 14:00:05 +0900

    ユーザ詳細・ユーザ一覧ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to stories_path
+      redirect_to user_path(@user.id)
     else
       render :new
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
   end
 
   private
