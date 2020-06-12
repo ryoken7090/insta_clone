37ba3983a20b047cb51d0ce39cd72b87f33f9bf9
ryoken nakayama <ryoken7090@gmail.com>
2020-06-12 11:10:05 +0900

    bootstrapの導入

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,10 +1,6 @@
 class Story < ApplicationRecord
-  validates :image_or_content, presence:true
-  private
-  def image_or_content
-    image.presence or content.presence
-  end
   mount_uploader :image, ImageUploader
+  validates :image,  presence: true
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
