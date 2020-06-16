d8f34392185d203d17a0d18769c1618f3ed48331
ryoken nakayama <ryoken7090@gmail.com>
2020-06-16 13:15:08 +0900

    rootをstories#indexに設定

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
+  root to: 'stories#index'
   resources :favorites, only: [:create, :destroy]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
